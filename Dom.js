6c31f8233aab552451cb03133099435ee48a308c
AravindSI <aravindsivankalai99>
2022-10-29 11:46:01 +0530

    second commit

---
+++ Dom.js
@@ -0,0 +1,13 @@
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.head);
+console.log(document.body);
+console.log(document.links);
+console.log(document.getElementById("header-title"));
+var header-title =document.getElementById("header-title");
+var header=document.getElementById("main-header");
+header-title.textContent="hello";
+header-title.innerText="good bye";
+header-title.Style.borderBottom="Solid 3px #000";
+
