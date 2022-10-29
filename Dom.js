efeaa4812058dfefa270774521dfc64cd013a222
AravindSI <aravindsivankalai99>
2022-10-29 16:18:51 +0530

    fifth commit

--- Dom.js
+++ Dom.js
@@ -1,7 +1,14 @@
-//getelementsByTagName
-var li=document.getElementsByTagName('li');
-console.log(li);     
-li[2].style.backgroundColor='green';
-for(var i=0;i<li.length;i++){
-    li[i].style.fontWeight = 'bold';
+//querySelector
+var input=document.querySelector('input');
+input.value='Hello World';
+var Submit=document.querySelector('input[type="Submit"]');
+Submit.value="Aravind";
+var item=document.querySelector(".list-group-item");
+item.style.color='red';
+var item=document.querySelector(".list-group-item:nth-child(2)");
+item.style.color='green';
+//queryselectorall
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
 }
